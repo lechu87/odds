49f3cca634c39a0265744464b1df7dec1f0f0431
Leszek Lesinski <lesin@interia.pl>
2018-11-02 20:48:37 +0100

    sending added

---
+++ run.sh
@@ -0,0 +1,10 @@
+source virtualenv/bin/activate
+
+python3 lvbet.py
+python3 sts.py
+python3 iforbet.py
+python3 fortuna.py
+python3 totolotek.py
+
+bash send.sh
+deactivate
---
+++ send.sh
@@ -0,0 +1,1 @@
+rsync -avzhe ssh db.sqlite  lesinle@ssh.pythonanywhere.com:/home/lesinle/web2py/applications/bestbukpl/databases/.
